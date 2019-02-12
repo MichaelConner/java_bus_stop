994a5ff18e41a63d0ec9bf2294bf4f26de2abc7f
Michael Conner <michaelconner@hotmail.co.uk>
2019-02-12 13:41:14 +0000

    initial commit

---
+++ src/main/java/Bus.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+
+public class Bus {
+
+    private String destination;
+    private int capacity;
+    private ArrayList<Person> passengerList;
+
+
+    public Bus(String destination, int capacity){
+        this.destination = destination;
+        this.capacity = capacity;
+        this.passengerList = new ArrayList<>();
+    }
+
+
+
+
+}
---
+++ src/test/java/BusTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class BusTest {
+
+    Bus bus;
+    Person person;
+
+
+    @Before
+
+
+
+
+
+
+
+    @Test
+
+
+
+
+
+
+
+
+
+}
