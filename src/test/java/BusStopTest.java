1343335bbe257c1959241a31a72c746bcec1b740
Michael Conner <michaelconner@hotmail.co.uk>
2019-02-12 14:34:44 +0000

    Add extensions 1-3

---
+++ src/test/java/BusStopTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusStopTest {
+
+
+    Bus bus;
+    Person person;
+    BusStop busStop;
+
+    @Before
+    public void before(){
+        bus = new Bus("The Jewel", 10);
+        person = new Person();
+        busStop = new BusStop("Hunter's Hall Park");
+    }
+
+    @Test
+    public void testQueueEmpty(){
+        assertEquals(0, busStop.queueSize());
+    }
+
+    @Test
+    public void testQueueHasPerson(){
+        busStop.addPerson(person);
+        assertEquals(1, busStop.queueSize());
+    }
+
+
+
+
+
+}
